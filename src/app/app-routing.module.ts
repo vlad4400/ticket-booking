8d87b1a313edf78b11816b62e64a461bcffcc3c9
vlad4400 <sleepvs@gmail.com>
2021-06-01 13:00:05 +0200

    create 'form2'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
     ]
   },
   {
-    path: "login",
+    path: "login/:departure/:arrives/:data/:tickets/:baggages",
     component: LoginComponent
   },
   {
--- src/app/components/main/form2/form2.component.ts
+++ src/app/components/main/form2/form2.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-form2',
@@ -8,12 +9,116 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class Form2Component implements OnInit {
 
-  ticketFrom: string = this.route.snapshot.params['from'];
-  ticketTo: string = this.route.snapshot.params['to'];
+  departure: string = this.activatedRoute.snapshot.params['departure'];
+  arrives: string = this.activatedRoute.snapshot.params['arrives'];
+  data: string = this.activatedRoute.snapshot.params['data'];
 
-  constructor(private route: ActivatedRoute) { }
+  plane :{
+    seat :number,
+    available :boolean
+  }[] = [];
+  planeType !: 1 | 2 | 3;
+  planeTypes = {
+    1: {
+      seats: 78
+    },
+    2: {
+      seats: 189
+    },
+    3: {
+      seats: 330
+    }
+  }
+  tickets :number[] = [];
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private route: Router,
+    private renderer: Renderer2
+  ) { }
 
   ngOnInit(): void {
+    switch (this.departure) {
+      case 'lodz':
+        switch (this.arrives) {
+          case 'kiev':
+            this.planeType = 3;
+            break;
+          case 'krakow':
+            this.planeType = 1;
+            break;
+          case 'odessa':
+            this.planeType = 2;
+            break;
+
+          default:
+            break;
+        }
+        break;
+      case 'warsaw':
+        switch (this.arrives) {
+          case 'kiev':
+            this.planeType = 2;
+            break;
+          case 'krakow':
+            this.planeType = 1;
+            break;
+          case 'odessa':
+            this.planeType = 3;
+            break;
+
+          default:
+            break;
+        }
+        break;
+
+      default:
+        break;
+    }
+
+    for (let i = 1; i <= this.planeTypes[this.planeType].seats; i++) {
+      this.plane.push({
+          seat: i,
+          available: !Math.floor(Math.random() * 2)   // to get boolean type: 50% - true, 50% - false;
+      });
+    }
+  }
+
+  pick(event :any) {
+    let e = event.srcElement.attributes.class.value;
+
+    if (e.split(' ').includes('available')) {
+      if (this.tickets.length < 7) {
+        this.renderer.removeClass(event.target, 'available');
+        this.renderer.addClass(event.target, 'reserve');
+        this.tickets = [...this.tickets, event.target.id.substring(5)];
+      } else {
+        alert('Ticket limit reached (max: 7)');
+      }
+    } else if (e.split(' ').includes('reserve')) {
+      let n = event.target.id.substring(5);
+      this.tickets = this.tickets.filter(value => {
+        return value != n;
+      });
+      this.renderer.removeClass(event.target, 'reserve');
+      this.renderer.addClass(event.target, 'available');
+    }
+  }
+
+  onSubmit(f: NgForm) {
+
+    if (this.tickets.length) {
+      let baggages :string[] = [];
+
+      baggages = this.tickets.map(value=>{
+        return f.value['bagaz-' + value]
+      });
+
+      let ticketsStr = this.tickets.join('-');
+      let baggagesStr = baggages.join('-');
+
+      this.route.navigate([`/login/${this.departure}/${this.arrives}/${this.data}/${ticketsStr}/${baggagesStr}`]);
+    }
   }
 
 }
