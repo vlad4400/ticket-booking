33cb74d5ef5485b23705985640609e665953f027
vlad4400 <sleepvs@gmail.com>
2021-06-01 17:56:25 +0200

    add 'temperature' to 'form1'

--- src/app/components/main/form1/form1.component.ts
+++ src/app/components/main/form1/form1.component.ts
@@ -10,12 +10,41 @@ import { Router } from '@angular/router';
 export class Form1Component implements OnInit {
 
   dateNow !:string;
+  temp !:number;
+  form = {
+    srcCountry: 'lodz',
+    selectedData: this.currentTime()
+  }
+  cnt :number = 1;
+
 
   constructor(private route:Router) { }
 
   ngOnInit(): void {
-    console.log('ngOnInit');
     this.dateNow = this.currentTime();
+    this.updateTemperature(this.form.srcCountry, this.cnt);
+  }
+
+  getDiffInDays(firstdate :string, seconddate :string) {
+    let dt1 = firstdate.split('-'),
+        dt2 = seconddate.split('-'),
+        one = new Date(+dt1[0], +dt1[1]-1, +dt1[2]),
+        two = new Date(+dt2[0], +dt2[1]-1, +dt2[2]);
+
+    let millisecondsPerDay = 1000 * 60 * 60 * 24;
+    let millisBetween = two.getTime() - one.getTime();
+    let days = millisBetween / millisecondsPerDay;
+
+    return Math.floor(days);
+  }
+
+  updateTemperature(currentCity :string, cnt :number) {
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&cnt=${cnt}&units=metric&appid=6c6951e0b5b7141a02703029688c5344`)
+      .then( resp => resp.json())
+      .then( data => {
+          this.temp = data.main.temp;
+      });
   }
 
   currentTime(){
@@ -25,6 +54,14 @@ export class Form1Component implements OnInit {
     const day = today.getDate();
 
     return `${year}-${(months < 9) ? '0' : ''}${months+1}-${(day < 10) ? '0' : ''}${day}`;
+  }
+
+  onChanges() {
+    this.cnt = 1 + this.getDiffInDays(this.dateNow, this.form.selectedData);
+
+    if (this.cnt <= 16) {
+      this.updateTemperature(this.form.srcCountry, this.cnt)
+    };
   }
 
   onSubmit({value: { departure, arrives, inputDate }} :NgForm) {
